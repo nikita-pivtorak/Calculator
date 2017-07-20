057eccbb883f31d6f1f6082e8eb61d8ce0bf0c7b
ninikita-pivtorak <katapulter.inc@gmail.com>
2017-07-20 23:31:28 +0300

    1

---
+++ src/Calculator/CalculatorMain.java
@@ -0,0 +1,44 @@
+package Calculator;
+
+
+import java.util.Scanner;
+
+public class CalculatorMain {
+
+    public static void main(String[] args) {
+        System.out.println("Input number 1");
+        int numberOne = new Scanner(System.in).nextInt();
+
+        System.out.println("Input number 2");
+        int numberTwo = new Scanner(System.in).nextInt();
+
+        System.out.println("Input type of operation");
+        String operationType = new Scanner(System.in).nextLine();
+
+        int result = -1000;
+        switch (operationType) {
+            case "+":
+                result = numberOne + numberTwo;
+                break;
+            case "-":
+                result = numberOne - numberTwo;
+                break;
+            case "*":
+                result = numberOne * numberTwo;
+                break;
+            case "/":
+                result = numberOne / numberTwo;
+                break;
+            case "%":
+                result = numberOne % numberTwo;
+                break;
+            default:
+                System.out.println("It's the wrong number!");
+                break;
+        }
+
+        String answer = "Your result is: " + result;
+        System.out.println(answer);
+
+    }
+}
